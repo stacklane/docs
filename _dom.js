aaafcd9b378f1a6f8db55b4871f86c7b80a19cba
Ryan Dewell <ryan@dewell.org>
2017-10-09 09:16:21 -0600

    Commit

--- _dom.js
+++ _dom.js
@@ -2,4 +2,4 @@ $each = function(cls, cb) { if (!cls.startsWith('.')) return; Array.prototype.fo
 $get = function(id){ return document.getElementById(id); }
 $on = function(id, name, cb) { id.startsWith('.') ? $each(id, function(e){ e.addEventListener(name, cb) }) : $get(id).addEventListener(name, cb) }
 $up = function(start, cls) { if (cls.startsWith(".")) { cls = cls.substring(1); } else { return null; } if (start.classList.contains(cls)) return start; while ((start = start.parentElement) && !start.classList.contains(cls)); return start; }
-$esc = function(s) { var div = document.createElement('div'); div.appendChild(document.createTextNode(str)); return div.innerHTML }
+$esc = function(s) { var div = document.createElement('div'); div.appendChild(document.createTextNode(s)); return div.innerHTML }
