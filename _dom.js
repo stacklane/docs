7039da8d3c19fb6190d15e2767d54e3018a26a46
Ryan Dewell <ryan@dewell.org>
2017-11-06 13:54:21 -0700

    Commit

--- _dom.js
+++ _dom.js
@@ -1,4 +1,4 @@
-$each = function(cls, cb) { cls.startsWith('.') ? Array.prototype.forEach.call(document.getElementsByClassName(cls.substring(1)), cb) : Array.prototype.forEach.call(document.getElementsByName(cls), cb); }
+$each = function(cls, cb) { cls.startsWith('.') ? Array.prototype.forEach.call(document.getElementsByClassName(cls.substring(1)), cb) : Array.prototype.forEach.call(document.getElementsByTagName(cls), cb); }
 $get = function(id){ return document.getElementById(id); }
 $on = function(id, name, cb) { id.startsWith('.') ? $each(id, function(e){ e.addEventListener(name, cb) }) : $get(id).addEventListener(name, cb) }
 $up = function(start, cls) { if (cls.startsWith(".")) { cls = cls.substring(1); } else { return null; } if (start.classList.contains(cls)) return start; while ((start = start.parentElement) && !start.classList.contains(cls)); return start; }
